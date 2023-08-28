b99c6c06d656d9b8f509ed082105080a579b7c29
YourZhou <362409381@qq.com>
2023-08-28 19:35:43 +0800

    111

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
@@ -0,0 +1,40 @@
+package com.xuecheng.content.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.AddCourseDto;
+import com.xuecheng.content.model.dto.CourseBaseInfoDto;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+
+/**
+ * <p>
+ * 课程基本信息 服务类
+ * </p>
+ *
+ * @author itcast
+ * @since 2022-10-07
+ */
+public interface CourseBaseInfoService extends IService<CourseBase> {
+    /*
+     * @description 课程查询接口
+     * @param pageParams 分页参数
+     * @param queryCourseParamsDto 条件条件
+     * @return com.xuecheng.base.model.PageResult<com.xuecheng.content.model.po.CourseBase>
+     * @author Mr.M
+     * @date 2022/9/6 21:44
+     */
+    PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto);
+
+    /**
+     * @description 添加课程基本信息
+     * @param companyId  教学机构id
+     * @param addCourseDto  课程基本信息
+     * @return com.xuecheng.content.model.dto.CourseBaseInfoDto
+     * @author Mr.M
+     * @date 2022/9/7 17:51
+     */
+    CourseBaseInfoDto createCourseBase(Long companyId, AddCourseDto addCourseDto);
+
+}
